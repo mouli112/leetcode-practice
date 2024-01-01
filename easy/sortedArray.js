6488f7d159a280ef34a84ab0e2a9f47b33c88e59
mouli112 <18kq1a0415@pace.ac.in>
2024-01-01 18:05:19 +0530

    added sortedArray

---
+++ easy/sortedArray.js
@@ -0,0 +1,14 @@
+var deleteDuplicates = function(head) {
+    if(head === null) return head;
+    let node  = head;
+    while(node && node.next) {
+        if(node.next.val === node.val) {
+            node.next = node.next.next
+        }else {
+            node = node.next
+        }
+    }
+    return head;
+};
+let head = [1,1,2];
+console.log(deleteDuplicates(head));
