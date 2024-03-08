c6e8960643972472ca3fed06ac5cccb4fcbc06e5
mouli112 <18kq1a0415@pace.ac.in>
2024-03-08 21:31:53 +0530

    find-a-chair problem added

---
+++ find-a-chair.js
@@ -0,0 +1,28 @@
+function findSpareChairs(rooms, need) {
+    let chairsTaken = [];
+    let totalChairsNeeded = need;
+
+    for (let i = 0; i < rooms.length; i++) {
+        const occupants = rooms[i][0].length;
+        const availableChairs = rooms[i][1] - occupants;
+
+        if (availableChairs > 0) {
+            if (availableChairs >= totalChairsNeeded) {
+                chairsTaken.push(totalChairsNeeded);
+                return chairsTaken;
+            } else {
+                chairsTaken.push(availableChairs);
+                totalChairsNeeded -= availableChairs;
+            }
+        } else {
+            chairsTaken.push(0);
+        }
+    }
+
+    return totalChairsNeeded === 0 ? chairsTaken : 0;
+}
+
+// Test case
+const rooms = [['XXX', 3], ['XXXXX', 6], ['XXXXXX', 9], ['XXX',2]];
+const need = 4;
+console.log(findSpareChairs(rooms, need)); // Output: [0, 1, 3]
